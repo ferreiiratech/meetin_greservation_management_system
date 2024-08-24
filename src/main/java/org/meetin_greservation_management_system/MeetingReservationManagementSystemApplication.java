a63adf94a174194a743973bf544dfb8edd6dd88a
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-08-24 19:09:50 -0300

    first commit

---
+++ src/main/java/org/meetin_greservation_management_system/MeetingReservationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.meetin_greservation_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingReservationManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingReservationManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/meetin_greservation_management_system/MeetingReservationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.meetin_greservation_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingReservationManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
